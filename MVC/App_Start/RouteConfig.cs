9e5a955482de13fc3a97c6c5cc343bad2e37fc5f
cutesiucap <trungtinbee@gmail.com>
2020-12-08 08:56:46 +0700

    commit template admin

--- MVC/App_Start/RouteConfig.cs
+++ MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Accounts", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] { "MVC.Controllers" }
             );
         }
--- MVC/Controllers/Admin/AdminController.cs
+++ MVC/Controllers/Admin/AdminController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +13,10 @@ namespace MVC.Controllers.Admin
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Accounts> accounts;
+            HttpResponseMessage httpResponseMessage = GlobalVariables.HttpClient.GetAsync("Accounts").Result;
+            accounts = httpResponseMessage.Content.ReadAsAsync<IEnumerable<Accounts>>().Result;
+            return View(accounts);
         }
     }
 }
